5f7d358db86fa09de3a1a483c364f7d6cacde2cb
Your Name <thalesfelippe>
2021-06-18 16:08:05 -0300

    home changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 
-    $(".project-carousel").slick({
+    $('.project-carousel').slick({
         dots: true,
         infinite: true,
         speed: 500,
         fade: true,
         cssEase: 'linear'
+      });
+
+    $(".project-carousel").on('change', function() {
+        $("#project-name").text($(".slick-current").data("name") );
+        $("#project-description").text($(".slick-current").data("description") );
     });
+
+    $('.project-carousel').on('afterChange',function( currentSlide ) {
+        $("#project-name").text($(".slick-current").data("name") );
+        $("#project-description").text($(".slick-current").data("description") );
+      });
 });
